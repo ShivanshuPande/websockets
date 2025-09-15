694f2efb5915efe35b26e143a1fbefe868cf8876
fable <pandeyshivanshu2005@gmail.com>
2025-09-15 12:43:09 +0530

    ws server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import WebSocket, { WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer , RawData } from "ws";
 import http, { IncomingMessage, ServerResponse } from 'http';
 
 
@@ -6,18 +6,18 @@ const server = http.createServer(function(request : IncomingMessage , response :
 
     // so i have added the types , i think this should be correct by now
     console.log(new Date()+ "Received Request For " + request.url);
-    response.end("Connection Established")
+    response.end("Connection Established , We can communicate now!!")
 
 }) ;
 
 const wss = new WebSocketServer({server});
 
 
-wss.on('connection' , function connection(ws){
-    ws.on('error' , console.error);
+wss.on('connection' , function connection(socket){
+    socket.on('error' , console.error);
 
 
-    ws.on('message' , function message(data : any , isBinary : any){
+    socket.on('message' , function message(data : RawData , isBinary : boolean ){
         wss.clients.forEach(function each(client){
             if(client.readyState === WebSocket.OPEN){
                 client.send(data , {binary : isBinary});
@@ -25,9 +25,9 @@ wss.on('connection' , function connection(ws){
         });
 
     });
+    console.log("something has established a connection through a websocket ")
 
-
-    ws.send("This is the message from the server!!")
+    socket.send("This is the message from the server!! , connection to the websocket has been established")
 
 });
 
